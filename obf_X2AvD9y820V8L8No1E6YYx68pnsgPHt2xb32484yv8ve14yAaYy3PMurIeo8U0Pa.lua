bbf20b432169e3e0cbba2ddad203717eb8109ceb
TgamingFake <122189589+TgamingFake@users.noreply.github.com>
2024-01-26 17:56:07 +0700

    Add files via upload

---
+++ obf_X2AvD9y820V8L8No1E6YYx68pnsgPHt2xb32484yv8ve14yAaYy3PMurIeo8U0Pa.lua
@@ -0,0 +1,5 @@
+_G.Name = "Duy Phong Hub" -- Put You Name Script Here
+
+_G.Logo = "16116284420" -- Enter Your ID Image Here
+
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
